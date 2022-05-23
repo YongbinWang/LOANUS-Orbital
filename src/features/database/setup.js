65d00714a04f61f338b592e275a6a830d35b76bb
Wang Haoyang <haoyangw@gmail.com>
2022-05-23 19:17:25 +0800

    Firebase setup: Don't export the app, but export the auth and db objects for downstream usage instead

--- src/features/database/setup.js
+++ src/features/database/setup.js
@@ -1,4 +1,6 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+​​import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD8KqFXSRWUb5ux0tfmFj8WdBAL2KWMsYU",
@@ -10,4 +12,6 @@ const firebaseConfig = {
     measurementId: "G-57EK5B21ZZ"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
