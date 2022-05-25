6539b7837404bc741630f826f56aebe7872c7ffa
Wang Haoyang <haoyangw@gmail.com>
2022-05-25 17:26:32 +0800

    Remove unused imports from NavigationBar component

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,12 +1,8 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { ReactComponent as ReactSearchLogo } from "../assets/searchicon.svg";
 import { useAuth } from "../database/auth";
 import SignInBtn from "./SignInBtn";
 import ProfileBtn from "./ProfileBtn";
-import { IconButton } from "@mui/material";
-import { AddCircleOutline } from "@mui/icons-material";
-import SearchIcon from "@mui/icons-material/Search";
 import CreateBtn from "./CreateBtn";
 import SearchBar from "./SearchBar";
 
