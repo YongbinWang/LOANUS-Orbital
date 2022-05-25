f8d5aaf37504ece564388291f8d3e1aa78fc7254
YongbinWang <yongbin_01@hotmail.com>
2022-05-25 23:45:30 +0800

    Set up backend connection to database

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+mongoose.connect(
+  "mongodb+srv://loanus123:loanushyyb123@loanus-database.csjkq.mongodb.net/loanusdatabase?retryWrites=true&w=majority"
+);
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+  res.json(user);
+});
+
+app.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
