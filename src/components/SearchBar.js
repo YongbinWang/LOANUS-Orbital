b2780cc166785c0037938d6f163ce290576a965a
Wang Haoyang <haoyangw@gmail.com>
2022-05-25 16:16:22 +0800

    Center align the search icon(used to be vertical aligned to the top)

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,22 @@
+import styled from "styled-components";
 import { TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
+const SearchBarDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
 export default function SearchBar() {
     return (
-        <div>
+        <SearchBarDiv>
             <TextField
                 id="search-bar"
                 hiddenLabel
                 defaultValue="Search"
                 variant="filled" />
             <SearchIcon />
-        </div>
+        </SearchBarDiv>
     );
 }
