d0c3cf679f144917a1a13aeb48f38ecb60dda63f
Wang Haoyang <haoyangw@gmail.com>
2022-05-25 18:06:26 +0800

    Format the navigation bar again

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,15 +5,16 @@ import SignInBtn from "./SignInBtn";
 import ProfileBtn from "./ProfileBtn";
 import CreateBtn from "./CreateBtn";
 import SearchBar from "./SearchBar";
+import { Button } from "@mui/material";
 
 const MainContainer = styled.nav`
   background-color: #2d3c4a;
-  width: 100vw;
   height: 10vh;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
+  padding: 0ex 1ex;
 `;
 
 const NavLink = styled(Link)`
@@ -22,11 +23,16 @@ const NavLink = styled(Link)`
   padding: 0ex 1ex;
 `;
 
+const HomeBtn = styled(Button)`
+  flex: 0 1 auto;
+  color: white;
+`;
+
 function NavigationBar() {
   const { user } = useAuth();
   return (
     <MainContainer>
-      <NavLink to="/home">Home Page</NavLink>
+      <HomeBtn component={ Link } to="/home">Home Page</HomeBtn>
       <SearchBar />
       <CreateBtn />
       { user ? <ProfileBtn> Profile</ProfileBtn> : <SignInBtn url='/signin' /> }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,11 @@
 import styled from "styled-components";
-import { TextField } from "@mui/material";
+import { IconButton, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBarDiv = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: 1ex;
     flex: 1 0 auto;
 `;
 
@@ -19,10 +18,11 @@ export default function SearchBar() {
         <SearchBarDiv>
             <GrowTextField
                 id="search-bar"
-                hiddenLabel
-                defaultValue="Search"
+                label="Search"
                 variant="filled" />
-            <SearchIcon />
+            <IconButton>
+                <SearchIcon />
+            </IconButton>
         </SearchBarDiv>
     );
 }
